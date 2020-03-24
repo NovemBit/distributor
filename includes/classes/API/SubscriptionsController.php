4d2ed8b3b9736e82ec3274e8f157eb834c8fa3fa
armdev17 <arman.hovhannisyan997@gmail.com>
2020-03-24 18:16:42 +0400

    Update terms on spoke only when new data received from hub

--- includes/classes/API/SubscriptionsController.php
+++ includes/classes/API/SubscriptionsController.php
@@ -270,9 +270,7 @@ class SubscriptionsController extends \WP_REST_Controller {
 			}
 
 			$last_update       = get_post_meta( $post_id, 'dt_subscription_update' );
-			$last_update_meta  = $last_update[0]['meta'];
 			$last_update_terms = $last_update[0]['terms'];
-			$last_update_media = $last_update[0]['media'];
 
 			update_post_meta( $post_id, 'dt_subscription_update', $update );
 
@@ -302,7 +300,7 @@ class SubscriptionsController extends \WP_REST_Controller {
 				\Distributor\Utils\set_meta( $post_id, $request['post_data']['distributor_meta'] );
 			}
 
-			if ( ! empty( $request['post_data']['distributor_terms'] ) ) {
+			if ( ! empty( $request['post_data']['distributor_terms'] ) && $update['terms'] != $last_update_terms ) {
 				\Distributor\Utils\set_taxonomy_terms( $post_id, $request['post_data']['distributor_terms'] );
 			}
 
